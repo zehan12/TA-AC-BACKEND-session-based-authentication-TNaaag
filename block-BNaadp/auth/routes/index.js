c7a13c59ac536aed43b2ea00053c5da392329619
Zehan Khan <zehan9211@gmail.com>
2022-03-04 22:29:42 +0530

    cookie with pug file

---
+++ block-BNaadp/auth/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' , message: "this is index page"});
+});
+
+module.exports = router;
